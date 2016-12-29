415a6ca9374cb8de4dde2abf76baf53cbb3948c0
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2016-12-29 18:42:31 +0000

    Add record view

--- records/views.py
+++ records/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Record
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+	return HttpResponse("Hello, world. You're at the index.")
 
+def record(request, record_id):
+	rec = Record.objects.get(id=record_id)
 
+	return HttpResponse("Record %s." % rec.currentName)
 
