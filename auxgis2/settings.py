90708ca18124ed431bdf634442af2d651fb36ce0
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2017-01-01 03:25:25 +0000

    Add user registration

--- auxgis2/settings.py
+++ auxgis2/settings.py
@@ -31,6 +31,9 @@ STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL  = "/"
+
 # Application definition
 
 INSTALLED_APPS = [
--- records/views.py
+++ records/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse, HttpResponseRedirect
 from django.template import loader
 from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.decorators import login_required
+from django.utils.datastructures import MultiValueDictKeyError
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from .models import Record, DatasetSnapshot, DatasetRecord
 import json
+import re
 
 def index(request):
 	recs = Record.objects.all()[:100]
@@ -28,6 +36,7 @@ def record(request, record_id):
 	template = loader.get_template('records/record.html')
 	return HttpResponse(template.render({"record": rec, 'datasetSeries': ds, 'snapshots': snapshots}, request))
 
+@login_required
 def record_edit(request, record_id):
 	rec = get_object_or_404(Record, id=record_id)
 	ds = rec.datasetSeries
@@ -60,13 +69,13 @@ def original_record(request, record_id, snapshot_id):
 def records_query(request):
 	try:
 		bbox = map(float, request.GET["bbox"].split(","))
-	except ValueError:
+	except (ValueError, MultiValueDictKeyError):
 		return HttpResponseBadRequest("Bad bbox")
 	try:
 		geomStr = 'POLYGON(( {0} {1}, {0} {3}, {2} {3}, {2} {1}, {0} {1} ))'.format(*bbox)
 	except IndexError:
 		return HttpResponseBadRequest("Bad bbox length")
-		
+	
 	geom = GEOSGeometry(geomStr, srid=4326)
 	recs = Record.objects.filter(currentPosition__within=geom)[:100]
 
@@ -80,4 +89,53 @@ def records_query(request):
 		output.append(jrec)
 
 	return JsonResponse(output, safe=False)	
+
+def logout_view(request):
+	logout(request)
+	return HttpResponseRedirect("/")
+
+def register(request):
+	actionMessage = None
+	reqUsername = ""
+	reqEmail = ""
+	reqPassword = ""
+	registerSuccess = False
+
+	try:
+		reqUsername = request.POST["username"]
+		reqEmail = request.POST["email"]
+		reqPassword = request.POST["password"]
+
+		validate_email(reqEmail)
+
+		if len(reqUsername) > 50:
+			raise ValueError("User name too long (limit is 50 characters)")
+		if len(reqPassword) > 1000:
+			raise ValueError("Password too long (limit is 1000 characters)")
+		if len(reqPassword) < 6:
+			raise ValueError("Password too short (limit is 6 characters)")
+
+		user = User.objects.create_user(reqUsername, reqEmail, reqPassword)
+		user.save()
+		if user is not None:
+			login(request, user)
+			actionMessage = "New user registered!"
+		else:
+			actionMessage = "New user registered! Automatic log in failed!"
+		registerSuccess = True
+	except MultiValueDictKeyError:
+		pass
+	except ValueError as err:
+		actionMessage = str(err)
+	except IntegrityError as err:
+		actionMessage = "User name is not unique"
+	except ValidationError as err:
+		actionMessage = "Error validating email"
+	
+	if not registerSuccess:
+		logout(request)
+	template = loader.get_template('registration/register.html')
+	return HttpResponse(template.render({"actionMessage": actionMessage, 
+		"reqUsername": reqUsername, "reqEmail": reqEmail, 
+		"reqPassword": reqPassword, "registerSuccess": registerSuccess}, request))
 
